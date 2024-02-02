0a9196f9db04e3e9fd48fff167120d8bb9575b59
Michal Sitek <michal.sitek2306@gmail.com>
2024-02-02 19:36:01 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
+var menuBtn = document.getElementById("menuBtn")
+        var sideNav = document.getElementById("sideNav")
+        var menu = document.getElementById("menu")
+
+        menuBtn.onclick = function(){
+            if(sideNav.style.right == "-250px"){
+                sideNav.style.right = "0";
+                menu.src = "images/close.png"
+            }
+            else{
+                sideNav.style.right = "-250px";
+                menu.src = "images/menu.png"
+            }
+        }
